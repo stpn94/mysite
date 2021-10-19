3fa1aaa5792cbcd36d0ebb8694dd3690a4f55940
stpn94 <stpn94@gmail.com>
2021-10-19 15:07:44 +0900

    exception practice #start

--- mysite03/src/main/java/com/douzone/mysite/repository/UserRepository.java
+++ mysite03/src/main/java/com/douzone/mysite/repository/UserRepository.java
@@ -15,56 +15,52 @@ public class UserRepository {
 
 	public UserVo findByEmailAndPassword(String email, String password) {
 		UserVo vo = null;
-		
+
 		Connection conn = null;
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
-				
+
 		try {
 			conn = getConnection();
-			
-			String sql =
-				" select no, name " + 
-			    "   from user " + 
-				"  where email=?" + 
-			    "    and password=?";
+
+			String sql = " select no, name " + "   from user " + "  where email=?" + "    and password=?";
 			pstmt = conn.prepareStatement(sql);
-			
+
 			pstmt.setString(1, email);
 			pstmt.setString(2, password);
-			
+
 			rs = pstmt.executeQuery();
-			
-			if(rs.next()) {
+
+			if (rs.next()) {
 				Long no = rs.getLong(1);
 				String name = rs.getString(2);
-				
+
 				vo = new UserVo();
 				vo.setNo(no);
 				vo.setName(name);
 			}
-			
+
 		} catch (SQLException e) {
 			System.out.println("error:" + e);
 		} finally {
 			try {
-				if(rs != null) {
+				if (rs != null) {
 					rs.close();
 				}
-				if(pstmt != null) {
+				if (pstmt != null) {
 					pstmt.close();
 				}
-				if(conn != null) {
+				if (conn != null) {
 					conn.close();
 				}
 			} catch (SQLException e) {
 				e.printStackTrace();
 			}
 		}
-		
+
 		return vo;
 	}
-	
+
 	public boolean insert(UserVo vo) {
 		boolean result = false;
 
@@ -72,74 +68,55 @@ public class UserRepository {
 		PreparedStatement pstmt = null;
 		try {
 			conn = getConnection();
-			
-			String sql =
-					" insert " + 
-					"   into user " + 
-					" values(null, ?, ?, ?, ?, now())";
+
+			String sql = " insert " + "   into user " + " values(null, ?, ?, ?, ?, now())";
 			pstmt = conn.prepareStatement(sql);
-			
+
 			pstmt.setString(1, vo.getName());
 			pstmt.setString(2, vo.getEmail());
 			pstmt.setString(3, vo.getPassword());
 			pstmt.setString(4, vo.getGender());
-			
+
 			int count = pstmt.executeUpdate();
-			result = count == 1;			
+			result = count == 1;
 		} catch (SQLException e) {
 			System.out.println("error:" + e);
 		} finally {
 			try {
-				if(pstmt != null) {
+				if (pstmt != null) {
 					pstmt.close();
 				}
-				if(conn != null) {
+				if (conn != null) {
 					conn.close();
 				}
 			} catch (SQLException e) {
 				e.printStackTrace();
 			}
-		}		
-		
+		}
+
 		return result;
-	}
-	
-	private Connection getConnection() throws SQLException {
-		Connection conn = null;
-		try {
-			Class.forName("org.mariadb.jdbc.Driver");
-			String url = "jdbc:mysql://127.0.0.1:3306/webdb?characterEncoding=utf8";
-			conn = DriverManager.getConnection(url, "webdb", "webdb");
-		} catch (ClassNotFoundException e) {
-			System.out.println("드라이버 로딩 실패:" + e);
-		} 
-		
-		return conn;
 	}
 
 	public UserVo findByNo(Long no) {
 		UserVo vo = null;
-		
+
 		Connection conn = null;
 		PreparedStatement pstmt = null;
 		ResultSet rs = null;
-				
+
 		try {
 			conn = getConnection();
-			
-			String sql =
-				" select no, name, email, gender " + 
-			    "   from user " + 
-				"  where no=?";
+
+			String sql = " select no, name, email, gender " + "   from user " + "  where no=?";
 			pstmt = conn.prepareStatement(sql);
-			
+
 			pstmt.setLong(1, no);
 
 			rs = pstmt.executeQuery();
-			
-			if(rs.next()) {
+
+			if (rs.next()) {
 				vo = new UserVo();
-				
+
 				vo.setNo(rs.getLong(1));
 				vo.setName(rs.getString(2));
 				vo.setEmail(rs.getString(3));
@@ -149,20 +126,20 @@ public class UserRepository {
 			System.out.println("error:" + e);
 		} finally {
 			try {
-				if(rs != null) {
+				if (rs != null) {
 					rs.close();
 				}
-				if(pstmt != null) {
+				if (pstmt != null) {
 					pstmt.close();
 				}
-				if(conn != null) {
+				if (conn != null) {
 					conn.close();
 				}
 			} catch (SQLException e) {
 				e.printStackTrace();
 			}
 		}
-		
+
 		return vo;
 	}
 
@@ -173,47 +150,54 @@ public class UserRepository {
 		PreparedStatement pstmt = null;
 		try {
 			conn = getConnection();
-			
-			if("".equals(vo.getPassword())) {
-				String sql =
-						" update user " + 
-						"    set name=?, gender=?" + 
-						"  where no=?";
+
+			if ("".equals(vo.getPassword())) {
+				String sql = " update user " + "    set name=?, gender=?" + "  where no=?";
 				pstmt = conn.prepareStatement(sql);
-				
+
 				pstmt.setString(1, vo.getName());
 				pstmt.setString(2, vo.getGender());
 				pstmt.setLong(3, vo.getNo());
 			} else {
-				String sql =
-						" update user " + 
-						"    set name=?, gender=?, password=?" + 
-						"  where no=?";
+				String sql = " update user " + "    set name=?, gender=?, password=?" + "  where no=?";
 				pstmt = conn.prepareStatement(sql);
-				
+
 				pstmt.setString(1, vo.getName());
 				pstmt.setString(2, vo.getGender());
 				pstmt.setString(3, vo.getPassword());
 				pstmt.setLong(4, vo.getNo());
 			}
-			
+
 			int count = pstmt.executeUpdate();
-			result = count == 1;			
+			result = count == 1;
 		} catch (SQLException e) {
 			System.out.println("error:" + e);
 		} finally {
 			try {
-				if(pstmt != null) {
+				if (pstmt != null) {
 					pstmt.close();
 				}
-				if(conn != null) {
+				if (conn != null) {
 					conn.close();
 				}
 			} catch (SQLException e) {
 				e.printStackTrace();
 			}
-		}		
-		
+		}
+
 		return result;
+	}
+
+	private Connection getConnection() throws SQLException {
+		Connection conn = null;
+		try {
+			Class.forName("org.mariadb.jdbc.Driver");
+			String url = "jdbc:mysql://127.0.0.1:3306/webdb?characterEncoding=utf8";
+			conn = DriverManager.getConnection(url, "webdb", "webdb");
+		} catch (ClassNotFoundException e) {
+			System.out.println("드라이버 로딩 실패:" + e);
+		}
+
+		return conn;
 	}
 }
