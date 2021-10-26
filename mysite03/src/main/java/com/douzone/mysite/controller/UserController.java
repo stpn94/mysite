49f2641f600fa8efa909b8aad0dc566c10568d8e
stpn94 <stpn94@gmail.com>
2021-10-26 10:28:38 +0900

    vaild #1

--- mysite03/src/main/java/com/douzone/mysite/controller/UserController.java
+++ mysite03/src/main/java/com/douzone/mysite/controller/UserController.java
@@ -1,8 +1,14 @@
 package com.douzone.mysite.controller;
 
+import java.util.List;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -24,8 +30,17 @@ public class UserController {
 	}
 
 	@RequestMapping(value = "/join", method = RequestMethod.POST)
-	public String join(UserVo vo) {
-		userService.join(vo);
+	public String join(@Valid UserVo vo, BindingResult result) {
+		if(result.hasErrors()) {
+			// BindingResult result는 Valid(검사)하고 난 결과
+			List<ObjectError> errlist = result.getAllErrors();
+			for(ObjectError error : errlist) {
+				System.out.println(error);
+			}
+			return "user/join";
+		}
+		
+//		userService.join(vo);
 		return "redirect:/user/joinsuccess";
 	}
 
